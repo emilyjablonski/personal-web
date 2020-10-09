a1ef5c2a082de4c1843fe1a071f06e044310a1dd
emily.jablonski <emily.jablonski@workday.com>
2020-10-09 13:49:11 -0600

    refactor: with more emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import './App.css';
+import styled from '@emotion/styled'
 
-function App() {
+const AppContainer = styled.div({
+  position: `absolute`,
+  top: 0,
+  left: 0,
+  backgroundColor: `#282c34`,
+  height: `100vh`,
+  width: `100vw`,
+  fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif`,
+  margin: 0
+})
+
+const AppContent = styled.div({
+  display: `flex`,
+  alignItems: `center`,
+  justifyContent: `center`,
+  height: `100%`,
+})
+
+const TextBox = styled.div({
+  display: `flex`,
+  alignItems: `center`,
+  justifyContent: `center`,
+  fontSize: `20px`,
+  color: `white`,
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        henlo it me emjabs
-        <br />
-        ▼・ᴥ・▼
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <AppContent>
+        <TextBox>
+          henlo it me emjabs
+          ▼・ᴥ・▼
+        </TextBox>
+      </AppContent>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
